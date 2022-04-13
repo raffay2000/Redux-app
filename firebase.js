13a864928e967f57b58d2f1a7f6411a9770728e7
Abdul Raffay <60923368+raffay2000@users.noreply.github.com>
2022-04-13 18:17:24 +0500

    Update firebase file

--- firebase.js
+++ firebase.js
@@ -1,22 +1,12 @@
-// Optionally import the services that you want to use
-//import {...} from "firebase/auth";
-//import {...} from "firebase/database";
-//import {...} from "firebase/firestore";
-//import {...} from "firebase/functions";
-//import {...} from "firebase/storage";
 
-  // initializeApp(firebaseConfig);
-  // Import the functions you need from the SDKs you need
-  // import { initializeApp, getApps } from "firebase/app";
-  // import { getAuth } from "firebase/auth";
-  import { initializeApp } from 'firebase/app'
-  import { getAuth } from 'firebase/auth'
-  import { getFirestore } from "firebase/firestore/lite";
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from "firebase/firestore/lite";
   
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyDlIMv7k9bgLY7T5Jqc6DH4_7r3z5qF-D8",
   authDomain: "fire-base-auth-5360b.firebaseapp.com",
@@ -29,15 +19,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const authentication = getAuth(app)
 export const db = getFirestore(app)
-
-
-// else{
-//     app= firebase.app()
-// }
-// let auth = firebase.auth()
-// export {auth}
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication and get a reference to the service
-// const auth = getAuth(app);
